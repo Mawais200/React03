690a198ad6d59bcc1808248420f13ce3e1203ec0
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-23 17:51:34 +0500

    run counter by useState

--- React03/src/assets/Button.jsx
+++ React03/src/assets/Button.jsx
@@ -14,16 +14,23 @@
 //             <p onMouseOver={Mouseover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, vel saepe velit quis quisquam nam iusto nemo enim odit officia dignissimos, ea nesciunt architecto illo obcaecati. Dolor ad nam veniam!</p>
 //            <button onDoubleClick={dblClick}>double click</button>
 //             </div>
+
+import { useState } from "react";
+
             
 //     )
 // }
 // useState//
 export default function Counter(){
-    let count=0;
-    function inCount(){
-        count=count+1;
-        console.log(count);
-    }
+    let [count,setCount]=useState(0);
+    // let count=0;
+    
+      let inCount = () =>{
+        setCount(count+1);
+      }
+        
+    
+    
     return(
         <>
         <h3>Count is {count}</h3>
