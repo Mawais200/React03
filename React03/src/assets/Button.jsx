d104af56efcc5feeaa1b7b0283706f8d193efca4
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-23 14:20:01 +0500

    no click event listner

--- React03/src/assets/Button.jsx
+++ React03/src/assets/Button.jsx
@@ -1,10 +1,20 @@
-function ClickMe(e) {
-    e.target.style.backgroundColor = "gold";
+function ClickMe(){
+    console.log("button clicked")
+}
+function Mouseover(){
+    console.log("mouse over")
+}
+function dblClick(){
+    console.log("double clicked")
 }
 export default  function Button(){
     return(
         <div>
             <button onClick={ClickMe}>click me</button>
+            <p onMouseOver={Mouseover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, vel saepe velit quis quisquam nam iusto nemo enim odit officia dignissimos, ea nesciunt architecto illo obcaecati. Dolor ad nam veniam!</p>
+           <button onDoubleClick={dblClick}>double click</button>
             </div>
+            
     )
 }
+ 
