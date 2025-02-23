ed8c373217054c0e99f7654a238eb0892c18ffd5
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-23 17:31:57 +0500

    use state

--- React03/src/App.jsx
+++ React03/src/App.jsx
@@ -1,4 +1,5 @@
-import Button from './assets/Button'
+// import Button from './assets/Button'
+import Counter from './assets/Button'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Button/>
+      <Counter/>
     </>
   )
 }
--- React03/src/assets/Button.jsx
+++ React03/src/assets/Button.jsx
@@ -1,20 +1,35 @@
-function ClickMe(){
-    console.log("button clicked")
-}
-function Mouseover(){
-    console.log("mouse over")
-}
-function dblClick(){
-    console.log("double clicked")
-}
-export default  function Button(){
-    return(
-        <div>
-            <button onClick={ClickMe}>click me</button>
-            <p onMouseOver={Mouseover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, vel saepe velit quis quisquam nam iusto nemo enim odit officia dignissimos, ea nesciunt architecto illo obcaecati. Dolor ad nam veniam!</p>
-           <button onDoubleClick={dblClick}>double click</button>
-            </div>
+// function ClickMe(){
+//     console.log("button clicked")
+// }
+// function Mouseover(){
+//     console.log("mouse over")
+// }
+// function dblClick(){
+//     console.log("double clicked")
+// }
+// export default  function Button(){
+//     return(
+//         <div>
+//             <button onClick={ClickMe}>click me</button>
+//             <p onMouseOver={Mouseover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, vel saepe velit quis quisquam nam iusto nemo enim odit officia dignissimos, ea nesciunt architecto illo obcaecati. Dolor ad nam veniam!</p>
+//            <button onDoubleClick={dblClick}>double click</button>
+//             </div>
             
+//     )
+// }
+// useState//
+export default function Counter(){
+    let count=0;
+    function inCount(){
+        count=count+1;
+        console.log(count);
+    }
+    return(
+        <>
+        <h3>Count is {count}</h3>
+        <button className="p-[5px_10px] text-black border-[1px] border-black" onClick={inCount}>click me</button>
+        </>
     )
+
 }
  
